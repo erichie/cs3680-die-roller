149dbe99035d943e52027935a02cb7d8a8e7b4f1
Edward Richards <e4.rich87@gmail.com>
2017-01-27 00:23:30 -0700

    added steppers for number of sides and number of dice, began implementation of the roll dice function

--- ER Die Roller/ViewController.swift
+++ ER Die Roller/ViewController.swift
@@ -9,17 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var sideCountLabel: UILabel!
+    @IBOutlet var diceCountLabel: UILabel!
+    @IBOutlet var diceTotalTitleLabel: UILabel!
+    @IBOutlet var totalLabel: UILabel!
+    
+    var sideCount: Int = 4
+    var diceCount: Int = 1
+    var diceResults: [Int] = []
+    var diceTotal: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // set the labels text equal to the minimum values
+        sideCountLabel.text = "4"
+        diceCountLabel.text = "1"
+        diceTotalTitleLabel.isHidden = true
+        totalLabel.isHidden = true
+    }
+    
+    @IBAction func changeSidecount(sender: UIStepper) {
+        self.sideCount = Int(sender.value)
+        self.sideCountLabel.text = Int(sender.value).description
+    }
+    
+    @IBAction func changeDicecount(sender: UIStepper) {
+        self.diceCount = Int(sender.value)
+        self.diceCountLabel.text = Int(sender.value).description
+    }
+    
+    @IBAction func rollDice(sender: UIButton) {
+        self.diceResults = []
+        for _ in 1...diceCount {
+            let diceRoll = Int(arc4random_uniform(UInt32(self.sideCount)) + 1)
+            self.diceResults.append(diceRoll)
+        }
+        self.diceTotal = self.diceResults.reduce(0, +)
+        totalLabel.text = String(self.diceTotal)
+        diceTotalTitleLabel.isHidden = false
+        totalLabel.isHidden = false
+        
+        print(self.diceResults, self.diceTotal)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 

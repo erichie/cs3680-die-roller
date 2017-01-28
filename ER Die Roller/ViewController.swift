c663e782d78b552c0c50c36d6d0131f151cbcbbf
Edward Richards <e4.rich87@gmail.com>
2017-01-27 23:28:04 -0700

    added the display of all dice results

--- ER Die Roller/ViewController.swift
+++ ER Die Roller/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet var diceCountLabel: UILabel!
     @IBOutlet var diceTotalTitleLabel: UILabel!
     @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var stack: UIStackView!
     
     var sideCount: Int = 4
     var diceCount: Int = 1
@@ -23,7 +24,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // set the labels text equal to the minimum values
+        // set the labels text equal to the minimum values and hide other labels
         sideCountLabel.text = "4"
         diceCountLabel.text = "1"
         diceTotalTitleLabel.isHidden = true
@@ -41,17 +42,34 @@ class ViewController: UIViewController {
     }
     
     @IBAction func rollDice(sender: UIButton) {
+        self.clearStack()
         self.diceResults = []
-        for _ in 1...diceCount {
+        
+        for i in 1...diceCount {
             let diceRoll = Int(arc4random_uniform(UInt32(self.sideCount)) + 1)
-            self.diceResults.append(diceRoll)
+            let diceNumLabel = "Dice #" + String(i) + ": "
+            self.diceResults.append(diceRoll) // add the dice roll to the array of dice results
+            
+            // create a label to display the dice results
+            let label = UILabel()
+            label.textAlignment = .center
+            label.text = diceNumLabel + String(diceRoll)
+            self.stack.addArrangedSubview(label) // add the label to the vertical stack view
         }
-        self.diceTotal = self.diceResults.reduce(0, +)
+
+        self.diceTotal = self.diceResults.reduce(0, +) // add up all the dice roll results
+        
+        // display all the labels and the sum of all the dice rolls
         totalLabel.text = String(self.diceTotal)
         diceTotalTitleLabel.isHidden = false
         totalLabel.isHidden = false
-        
-        print(self.diceResults, self.diceTotal)
+    }
+    
+    // clear the stack view of all the labels from the previous dice roll
+    func clearStack() {
+        for view in self.stack.subviews {
+            view.removeFromSuperview()
+        }
     }
 
     override func didReceiveMemoryWarning() {
